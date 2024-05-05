767d7353bca9068ecbc92cab956d7e73c59a6816
kiruha3 <kiruha3@mail.ru>
2024-05-05 16:26:46 +0700

    *last commit

--- src/test/java/com/gridnine/testing/servise/FilteredFlightsServiceImplTest.java
+++ src/test/java/com/gridnine/testing/servise/FilteredFlightsServiceImplTest.java
@@ -83,9 +83,9 @@ class FilteredFlightsServiceImplTest {
         final FilteredFlightsServiceImpl filteredFlightsServicelmpl = new FilteredFlightsServiceImpl();
         LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
         List<Flight> listFlighWithEraseFlightsWhereLandingMoreTwwoHoudrs = Arrays.asList(
-                createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3),threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(9),threeDaysFromNow.plusHours(23), threeDaysFromNow.plusHours(25)),
+                createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3),threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(9),threeDaysFromNow.plusHours(23), threeDaysFromNow.plusHours(25)),
                 createFlight(threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4)),
-                createFlight(threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(5)),
+                createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(5)),
                 createFlight(threeDaysFromNow.minusHours(4), threeDaysFromNow.plusHours(6),threeDaysFromNow.plusHours(7), threeDaysFromNow.plusHours(9)),
                 createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3)),
                 createFlight(threeDaysFromNow.minusHours(3), threeDaysFromNow.plusHours(4),threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(7)),
@@ -93,15 +93,17 @@ class FilteredFlightsServiceImplTest {
                 createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3),threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(9),threeDaysFromNow.plusHours(23), threeDaysFromNow.plusHours(25))
         );
 
-                List<Flight> listFlighWithoutEraselightsWhereLandingMoreTwoHoudrs = Arrays.asList(
-                        createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3),threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(9),threeDaysFromNow.plusHours(23), threeDaysFromNow.plusHours(25)),
-                        createFlight(threeDaysFromNow.minusHours(4), threeDaysFromNow.plusHours(6),threeDaysFromNow.plusHours(7), threeDaysFromNow.plusHours(9)),
-                        createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3)),
-                        createFlight(threeDaysFromNow.minusHours(3), threeDaysFromNow.plusHours(4),threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(7)),
-                        createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3),threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(9),threeDaysFromNow.plusHours(23), threeDaysFromNow.plusHours(25))
-                );
+        List<Flight> listFlighWithoutEraselightsWhereLandingMoreTwoHoudrs = Arrays.asList(
+                createFlight(threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4)),
+                createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(5)),
+                createFlight(threeDaysFromNow.minusHours(4), threeDaysFromNow.plusHours(6),threeDaysFromNow.plusHours(7), threeDaysFromNow.plusHours(9)),
+                createFlight(threeDaysFromNow.plusHours(2), threeDaysFromNow.plusHours(3)),
+                createFlight(threeDaysFromNow.minusHours(3), threeDaysFromNow.plusHours(4),threeDaysFromNow.plusHours(4), threeDaysFromNow.plusHours(7)),
+                createFlight(threeDaysFromNow.minusHours(2), threeDaysFromNow.minusHours(5))
+        );
 
-        List<Flight> actualList =  filteredFlightsServicelmpl.eraselightsWhereLandingMoreTwwoHoudrs(listFlighWithEraseFlightsWhereLandingMoreTwwoHoudrs);
+
+        List<Flight> actualList =  filteredFlightsServicelmpl.eraselightsWhereLandingMoreTwwoHoudrs(listFlighWithEraseFlightsWhereLandingMoreTwwoHoudrs,"text");
         List<Flight> exceptedList =  listFlighWithoutEraselightsWhereLandingMoreTwoHoudrs;
         assertEquals(exceptedList.size(), actualList.size());
 
